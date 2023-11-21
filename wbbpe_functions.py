8f5c11ea3580d2fd63de095804c36d24e59d615e
Sagkun343 <135689643+Sagkun343@users.noreply.github.com>
2023-11-21 23:44:40 +0530

    Update wbbpe_functions.py

--- wbbpe_functions.py
+++ wbbpe_functions.py
@@ -46,10 +46,6 @@ def is_less_than_equals_value(numbers: list[int], threshold: int) -> tuple:
 
 
 def csv_to_list_of_integers(csv: str) -> list[int]:
-    """
-
-    :rtype: object
-    """
     res = []
     temp = str()
     num_set = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
